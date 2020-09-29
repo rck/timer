cec1e7a97e96c8f4af13b6cfc7b748f009b9dd98
Roland Kammerer <dev.rck@gmail.com>
2020-09-29 11:29:51 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	var (
+		flagBell       = flag.Bool("b", false, "Send bell")
+		flagNotifySend = flag.String("n", "", "Send desktop notification with given title (needs 'notify-send')")
+	)
+	flag.Parse()
+
+	var sleep time.Duration
+	for _, f := range flag.Args() {
+		d, err := time.ParseDuration(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+		sleep += d
+	}
+	end := time.Now().Add(sleep)
+
+	stdin := make(chan struct{}, 1)
+	go func() {
+		for {
+			fmt.Scanln()
+			stdin <- struct{}{}
+		}
+	}()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGUSR1, syscall.SIGTTIN)
+	// syscall.SIGUSR1: write current info
+	// syscall.SIGTTIN: ignore, we get it on `timer ... &`
+	go func() {
+		for {
+			select {
+			case sig := <-sigs:
+				if sig == syscall.SIGUSR1 {
+					status := status(end)
+					notifyStderr(status)
+					maybeNotifyDesktop(*flagNotifySend, status)
+				}
+			case <-stdin:
+				notifyStderr(status(end))
+			}
+		}
+	}()
+
+	// really does not matter, but recalc
+	time.Sleep(end.Sub(time.Now()))
+
+	if *flagBell {
+		fmt.Print("\a")
+	}
+	maybeNotifyDesktop(*flagNotifySend, fmt.Sprintf("%v", sleep))
+}
+
+func status(end time.Time) string {
+	return fmt.Sprintf("%s left, sleeping till %s\n", end.Sub(time.Now()).Round(time.Second).String(), end.Format("15:04:05"))
+}
+
+func notifyStderr(status string) {
+	fmt.Fprintln(os.Stderr, status)
+}
+func notifyDesktop(summary, status string) {
+	exec.Command("notify-send", summary, status).Run()
+}
+func maybeNotifyDesktop(summary, status string) {
+	if summary != "" {
+		notifyDesktop(summary, status)
+	}
+}
